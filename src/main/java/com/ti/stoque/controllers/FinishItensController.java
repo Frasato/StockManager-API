a959f9c21533d541a9ae6947762fe00a0c002159
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-04 12:20:27 -0300

    Create End Points for all itens

--- src/main/java/com/ti/stoque/controllers/FinishItensController.java
+++ src/main/java/com/ti/stoque/controllers/FinishItensController.java
@@ -1,7 +1,23 @@
 package com.ti.stoque.controllers;
 
+import com.ti.stoque.models.FinishItensModel;
+import com.ti.stoque.services.FinishItensService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/finish")
 public class FinishItensController {
+
+    @Autowired
+    private FinishItensService finishItensService;
+
+    @GetMapping(value = "/all")
+    public List<FinishItensModel> findAll(){
+        return finishItensService.findAl();
+    }
 }
