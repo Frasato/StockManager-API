81bbfcaf288cd1af424a3b8a1c9948934de8e83d
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-04 12:18:45 -0300

    Change search, changes itens on updateItensOnDBOutTB and create LocalDateTime now for objects

--- src/main/java/com/ti/stoque/services/FinishItensService.java
+++ src/main/java/com/ti/stoque/services/FinishItensService.java
@@ -1,13 +1,20 @@
 package com.ti.stoque.services;
 
+import com.ti.stoque.models.FinishItensModel;
 import com.ti.stoque.repositories.FinishItensRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FinishItensService {
 
     @Autowired
     private FinishItensRepository finishItensRepository;
+
+    public List<FinishItensModel> findAl(){
+        return finishItensRepository.findAll();
+    }
 
 }
