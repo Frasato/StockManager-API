25c599ab5a40a50343d4b8699b897655aca437f1
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-04 12:20:06 -0300

    Create End Points for access

--- src/main/java/com/ti/stoque/controllers/ActiveItensController.java
+++ src/main/java/com/ti/stoque/controllers/ActiveItensController.java
@@ -1,7 +1,44 @@
 package com.ti.stoque.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ti.stoque.models.ActiveItensModel;
+import com.ti.stoque.models.FinishItensModel;
+import com.ti.stoque.services.ActiveItensService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/itens")
 public class ActiveItensController {
+
+    @Autowired
+    private ActiveItensService activeItensService;
+
+    @GetMapping(value = "/all")
+    public List<ActiveItensModel> showAllItens(){
+        return activeItensService.findAllItens();
+    }
+
+    @GetMapping(value = "/find/{seacrhItens}")
+    public List<ActiveItensModel> searchingItensFromTable(@PathVariable(name = "seacrhItens") String seacrhItens){
+        return activeItensService.findBySearchInput(seacrhItens);
+    }
+
+    @PostMapping(value = "/create")
+    public ActiveItensModel createItem(@RequestBody ActiveItensModel activeItensModel){
+        return activeItensService.createItensOnDB(activeItensModel);
+    }
+
+    @PutMapping(value = "/update-item/{id}")
+    public ActiveItensModel updateItem(@PathVariable(name = "id") UUID id, @RequestBody ActiveItensModel activeItensModel){
+        return activeItensService.updateItensOnDB(activeItensModel, id);
+    }
+
+    @PutMapping(value = "/takeout/{id}")
+    public ActiveItensModel takeOutItem(@PathVariable(name = "id") UUID id, @RequestBody ActiveItensModel activeItensModel, @RequestBody int removedAmount, @RequestBody String city, @RequestBody String sector){
+        return activeItensService.updateItensOnDBOutTB(activeItensModel, id, removedAmount, city, sector);
+    }
+
 }
