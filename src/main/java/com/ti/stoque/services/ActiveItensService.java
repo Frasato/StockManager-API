67c53e5c69fbe7c8216434d031bc7fcae4b28e53
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-09-06 15:35:06 -0300

    Remove inused imports and localDateTime

--- src/main/java/com/ti/stoque/services/ActiveItensService.java
+++ src/main/java/com/ti/stoque/services/ActiveItensService.java
@@ -7,7 +7,9 @@ import com.ti.stoque.repositories.FinishItensRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 
@@ -53,7 +55,6 @@ public class ActiveItensService {
         finishItensModel.setAmount(removedAmount);
         finishItensModel.setCityName(city);
         finishItensModel.setSectorName(sector);
-        finishItensModel.setOutDate(LocalDateTime.now());
 
         finishItensRepository.save(finishItensModel);
 
@@ -69,7 +70,6 @@ public class ActiveItensService {
         item.setItemName(activeItensModel.getItemName());
         item.setMarkName(activeItensModel.getMarkName());
         item.setAmount(activeItensModel.getAmount());
-        item.setCreatedDate(LocalDateTime.now());
 
         return activeitensRepository.save(item);
     }
